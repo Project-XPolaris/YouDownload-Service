ae9b57a31bcd56fe91bac2c7138d69bf47004c63
AllenTom <qweasdzxczse2@gmail.com>
2021-03-24 17:57:08 +0800

    update

--- api/template.go
+++ api/template.go
@@ -1,6 +1,8 @@
 package api
 
-import "github.com/projectxpolaris/youdownload-server/engine"
+import (
+	"github.com/projectxpolaris/youdownload-server/engine"
+)
 
 var taskStatusMapping map[engine.TaskStatus]string = map[engine.TaskStatus]string{
 	engine.Estimate:    "Estimate",
@@ -10,14 +12,15 @@ var taskStatusMapping map[engine.TaskStatus]string = map[engine.TaskStatus]strin
 }
 
 type BaseTaskTemplate struct {
-	Id       string  `json:"id"`
-	Name     string  `json:"name"`
-	Complete int64   `json:"complete"`
-	Length   int64   `json:"length"`
-	Progress float64 `json:"progress"`
-	Status   string  `json:"status"`
-	Speed    int64   `json:"speed"`
-	ETA      int64   `json:"eta"`
+	Id       string      `json:"id"`
+	Name     string      `json:"name"`
+	Complete int64       `json:"complete"`
+	Length   int64       `json:"length"`
+	Progress float64     `json:"progress"`
+	Status   string      `json:"status"`
+	Speed    int64       `json:"speed"`
+	ETA      int64       `json:"eta"`
+	Extra    interface{} `json:"extra"`
 }
 
 func (t *BaseTaskTemplate) Serializer(dataModel interface{}, context map[string]interface{}) error {
@@ -34,5 +37,57 @@ func (t *BaseTaskTemplate) Serializer(dataModel interface{}, context map[string]
 	if t.Speed != 0 {
 		t.ETA = task.Length() / t.Speed
 	}
+	if torrentTask, ok := task.(*engine.TorrentTask); ok {
+		extra := TorrentTaskExtra{}
+		extra.Assign(torrentTask)
+		t.Extra = extra
+	}
 	return nil
+}
+
+type TorrentTaskExtra struct {
+	Files []TorrentFile `json:"files"`
+	Peer  []TorrentPeer `json:"peer"`
+}
+
+type TorrentFile struct {
+	Name     string `json:"name"`
+	Length   int64  `json:"length"`
+	Path     string `json:"path"`
+	Priority int    `json:"priority"`
+}
+
+type TorrentPeer struct {
+	ClientName string `json:"clientName"`
+	Network    string `json:"network"`
+	Remote     string `json:"remote"`
+	Port       int    `json:"port"`
+}
+
+func (t *TorrentTaskExtra) Assign(task *engine.TorrentTask) {
+	if task.Torrent == nil {
+		return
+	}
+	if task.Torrent.Files() != nil {
+		t.Files = []TorrentFile{}
+		for _, file := range task.Torrent.Files() {
+			file := TorrentFile{
+				Name:     file.DisplayPath(),
+				Length:   file.Length(),
+				Path:     file.Path(),
+				Priority: int(file.Priority()),
+			}
+			t.Files = append(t.Files, file)
+		}
+	}
+	t.Peer = []TorrentPeer{}
+	for _, conn := range task.Torrent.PeerConns() {
+		peer := TorrentPeer{
+			ClientName: conn.PeerClientName,
+			Network:    conn.Network,
+			Remote:     conn.RemoteAddr.String(),
+			Port:       conn.PeerListenPort,
+		}
+		t.Peer = append(t.Peer, peer)
+	}
 }
